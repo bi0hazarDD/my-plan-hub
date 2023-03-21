7ec75e874ae14a7007f232b8dffc7313064b3892
bi0hazarDD <41805267+bi0hazarDD@users.noreply.github.com>
2023-03-21 14:16:23 +0000

    Initial Commit

---
+++ MyPlanHub/MyPlanHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPlanHubApp.swift
+//  MyPlanHub
+//
+//  Created by Johny's Macbook Pro 😏👌 on 21/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyPlanHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
